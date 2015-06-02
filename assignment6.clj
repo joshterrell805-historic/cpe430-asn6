e673c4968a505c8c7c7c838c95ac83c4dbe66594
Benjamin DiGregorio <ben@digregor.io>
2015-06-02 15:33:07 -0700

    separate serialize definition and test

--- assignment6.clj
+++ assignment6.clj
@@ -2,7 +2,7 @@
 
 (defprotocol ExcrC
   (foo [this]))
-  
+
 (defrecord NumC [^Integer n]
   ExcrC)
 (defrecord TrueC []
@@ -13,19 +13,21 @@
 (def n (new NumC 4))
 (println (instance? NumC n))
 
-(with-test
-  (defn serialize ^String [^::ExcrC ex]
-    "serialize an ExcrC"
-    (cond
-      (instance? NumC ex) (str (:n ex))
-      (instance? TrueC ex) "true"
-      (instance? FalseC ex) "false"
-      :else (throw (new Exception "serialize requires an ExcrC"))))
+(defn serialize ^String [^::ExcrC ex]
+  "serialize an ExcrC"
+  (cond
+    (instance? NumC ex) (str (:n ex))
+    (instance? TrueC ex) "true"
+    (instance? FalseC ex) "false"
+    :else (throw (new Exception "serialize requires an ExcrC"))))
+
+(deftest testSerialize
   (is (= "true" (serialize (new TrueC))))
   (is (= "false" (serialize (new FalseC))))
   (is (= "4" (serialize (new NumC 4))))
   (is (= "-12" (serialize (new NumC -12))))
   (is (thrown-with-msg? Exception #"serialize requires an ExcrC"
       (serialize "asdf"))))
+
 
 (run-tests)
